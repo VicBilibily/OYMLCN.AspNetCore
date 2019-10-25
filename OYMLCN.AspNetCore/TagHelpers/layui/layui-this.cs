1a838f2b6bda4cfc7214fb36b2f9edcb54ea634c
VicBilibily <mail@oyml.cn>
2019-10-25 17:28:40 +0800

    为LayuiThisTagHelper添加更多属性

--- OYMLCN.AspNetCore/TagHelpers/layui/layui-this.cs
+++ OYMLCN.AspNetCore/TagHelpers/layui/layui-this.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 using OYMLCN.Extensions;
+using System.Collections.Generic;
 
 namespace Microsoft.AspNetCore.Mvc.TagHelpers
 {
@@ -9,8 +10,17 @@ namespace Microsoft.AspNetCore.Mvc.TagHelpers
     /// </summary>
     [HtmlTargetElement("li", Attributes = "layui-this-controller")]
     [HtmlTargetElement("dd", Attributes = "layui-this-controller,layui-this-action")]
+    [HtmlTargetElement("dd", Attributes = "layui-this-controller,layui-this-action")]
+    [HtmlTargetElement("a", Attributes = "layui-this")]
+    [HtmlTargetElement("a", Attributes = "asp-all-route-data,layui-all-route-data")]
+    [HtmlTargetElement("a", Attributes = "asp-route-*,layui-route-*")]
     public class LayuiThisTagHelper : ViewContextTagHelper
     {
+        /// <summary>
+        /// layui-this
+        /// </summary>
+        [HtmlAttributeName("layui-this")]
+        public bool? LayuiThis { get; set; }
         /// <summary>
         /// layui-this-controller
         /// </summary>
@@ -23,13 +33,39 @@ namespace Microsoft.AspNetCore.Mvc.TagHelpers
         public string Action { get; set; }
 
         /// <summary>
+        /// asp-all-route-data
+        /// </summary>
+        [HtmlAttributeName("asp-all-route-data", DictionaryAttributePrefix = "asp-route-")]
+        public IDictionary<string, string> RouteValues { get; set; } = new Dictionary<string, string>();
+        /// <summary>
+        /// layui-all-route-data
+        /// </summary>
+        [HtmlAttributeName("layui-all-route-data", DictionaryAttributePrefix = "layui-route-")]
+        public IDictionary<string, string> LayuiRouteValues { get; set; } = new Dictionary<string, string>();
+
+        private bool CheckRouteValues()
+        {
+            if (LayuiRouteValues.Count == 0) return false;
+            foreach (var rule in LayuiRouteValues)
+            {
+                if (RouteValues.TryGetValue(rule.Key, out string source) && Equals(rule.Value, source))
+                    continue;
+                else
+                    return false;
+            }
+            return true;
+        }
+        /// <summary>
         /// Process
         /// </summary>
         /// <param name="context"></param>
         /// <param name="output"></param>
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
-            if (IsEqualController(Controller) && (Action.IsNullOrEmpty() || IsEqualAction(Action)))
+            if (LayuiThis == true ||
+                IsEqualController(Controller) && (Action.IsNullOrEmpty() || IsEqualAction(Action)) ||
+                CheckRouteValues()//LayuiRouteValues.Count > 0 && LayuiRouteValues.IsFullEqual(RouteValues)
+                )
                 output.AddClass("layui-this");
             base.Process(context, output);
         }
